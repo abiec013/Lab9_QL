03b1ac7a37674111e99f6b494cefb5fa67ded364
abiec013 <aec1020121@yahoo.com>
2017-10-07 17:39:19 -0400

    first commit

---
+++ Lab9/Circle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    class Circle
+    {
+        
+        public double radius;
+
+
+
+
+        public double Radius
+
+        { 
+            set { radius = value; }
+
+            get { return radius; }
+
+        }
+
+        public double Circumferance ()
+        {
+            
+            return (Math.PI * 2 * radius);
+        }
+
+        public  double GetArea ()
+
+        {
+
+            return (Math.PI * Math.Pow(radius, 2));
+            
+
+        }
+
+
+      
+    }
+}
---
+++ Lab9/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Count = 0;
+            bool programContinues = true;
+            while (programContinues == true)
+            {
+
+               
+                Circle newcircle = new Circle();
+                Count++;
+
+                Console.WriteLine("Welcome to the Circle test!");
+                Console.WriteLine("Enter radius");
+                newcircle.radius = double.Parse(Console.ReadLine());
+
+                if (newcircle.radius < 0)
+                {
+
+                }
+                    
+                Console.WriteLine("This is the circumference " + newcircle.Circumferance());
+                Console.WriteLine("This is the area " + newcircle.GetArea());
+
+                Console.WriteLine("Continue? y/n");
+                string userChoice = Console.ReadLine();
+                if (userChoice == "y")
+                {
+                    programContinues = true;
+                }
+                else
+                {
+                   
+                    
+                    programContinues = false;
+                    Console.WriteLine("Bye Bye" + " you have created " + " " + Count + " objects");
+
+
+                }
+
+            }
+
+        }
+    }
+}
